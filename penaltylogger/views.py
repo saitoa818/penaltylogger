b80c3e2c94134cf72d0682a0b1c4194a368b41b2
saitouakira <saitoa@cheered.jp>
2020-02-13 18:02:23 +0900

    チュートリアルを元にviewsとテンプレート作成。formの反映は確認。 playerの名前が出来ていないので、挙動が確認できない。 playerを外して作業すると、保存が出来ない状態。 views.pyのredirectの参照値がない。

--- penaltylogger/views.py
+++ penaltylogger/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Log
+from django.shortcuts import render, get_object_or_404
+from .forms import LogForm
 
-# Create your views here.
- 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'penaltylogger/post_list.html', {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Log, pk=pk)
+    return render(request, 'penaltylogger/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = LogForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = LogForm()
+    return render(request, 'penaltylogger/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Log, pk=pk)
+    if request.method == "POST":
+        form = LogForm(request.Log, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = LogForm(instance=post)
+    return render(request, 'penaltylogger/post_edit.html', {'form': form})
