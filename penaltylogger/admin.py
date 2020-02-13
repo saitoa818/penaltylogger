680570b5b3d733edafb05484f270ce38d24d2f60
saitouakira <saitoa@cheered.jp>
2020-02-13 20:05:54 +0900

    ■変更箇所 ・forms.pyにラベルを振った ・models.pyのLog classのplayerの内容を変更 ・データベースにmodelsが対応していなかったのでデータベースの再作成 ■修正予定箇所 ・penaltyoobject1と表示されるのでペナルティごとの名前に変更。違反の内容も同じく ・views.pyのredirectの参照値がない。 ・Error部分 ■Error ・入力したものを保存しようとすると「OperationalError」がでる

--- penaltylogger/admin.py
+++ penaltylogger/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Log
+from .models import Penalty
+from .models import Violation
 
 admin.site.register(Log)
+admin.site.register(Penalty)
+admin.site.register(Violation)
