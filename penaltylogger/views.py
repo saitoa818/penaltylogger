9a12c0c30e85dbd19d5fb04b7ba2e43d3759cf8d
saitouakira <saitoa@cheered.jp>
2020-03-04 13:44:41 +0900

    確認用

--- penaltylogger/views.py
+++ penaltylogger/views.py
@@ -14,21 +14,34 @@ def log_detail(request, pk):
     log = get_object_or_404(Log, pk=pk)
     return render(request, 'penaltylogger/log_detail.html', {'log': log})
 
-def log_new(request):
-    if request.method == "POST":
-        form = LogForm(request.POST)
+
+#def log_new(request):
+    # if request.method == "POST":
+    #     form = LogForm(request.POST)
+    #     if form.is_valid():
+    #         log = form.save(commit=False)
+    #         log.author = request.user
+    #         log.published_date = timezone.now()
+    #         log.save()
+    #         return redirect('log_detail', pk=log.pk)
+    # else:
+    #     form = LogForm()
+    # return render(request, 'penaltylogger/log_edit.html', {'form': form})
+def log_new(request, pk):
+    if request.method == "GET":
+        form = LogForm(request.GET)
         if form.is_valid():
             log = form.save(commit=False)
-            log.author = request.user
-            log.published_date = timezone.now()
+            player = Player.objects.filter(player_no=form.data['player_no']).first()
+            log.player = player
             log.save()
-            return redirect('log_detail', pk=log.pk)
+            return redirect('log_confirm', pk=log.pk)
     else:
         form = LogForm()
-    return render(request, 'penaltylogger/log_edit.html', {'form': form})
+    return render(request, 'penaltylogger/log_new.html', {'form': form})
 
 # 投稿確認画面（プレビュー）を表示
-def log_preview(request):
+def log_confirm(request):
     if request.method == "POST":
         form = LogForm(request.POST)
         if form.is_valid():
@@ -39,16 +52,16 @@ def log_preview(request):
                 context=context
             )
 
-def log_edit(request, pk):
-    log = get_object_or_404(Log, pk=pk)
-    if request.method == "POST":
-        form = LogForm(request.POST, instance=log)
-        if form.is_valid():
-            log = form.save(commit=False)
-            player = Player.objects.filter(player_no=form.data['player_no']).first()
-            log.player = player
-            log.save()
-            return redirect('log_detail', pk=log.pk)
-    else:
-        form = LogForm(instance=log)
-    return render(request, 'penaltylogger/log_preview.html', {'form': form})
+# def log_edit(request, pk):
+#     log = get_object_or_404(Log, pk=pk)
+#     if request.method == "POST":
+#         form = LogForm(request.POST, instance=log)
+#         if form.is_valid():
+#             log = form.save(commit=False)
+#             player = Player.objects.filter(player_no=form.data['player_no']).first()
+#             log.player = player
+#             log.save()
+#             return redirect('log_detail', pk=log.pk)
+#     else:
+#         form = LogForm(instance=log)
+#     return render(request, 'penaltylogger/log_confirm.html', {'form': form})
