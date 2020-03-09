028726aa9436dd5c234e6cdd675214211a98598c
saitouakira <saitoa@cheered.jp>
2020-03-09 12:35:14 +0900

    確認用

--- penaltylogger/urls.py
+++ penaltylogger/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -6,6 +7,6 @@ urlpatterns = [
     path('log/<int:pk>/', views.log_detail, name='log_detail'),
     path('log/new/', views.log_new, name='log_new'),
     #path('log/<int:pk>/edit/', views.log_edit, name='log_edit'),
-    path('log/confirm/', views.log_confirm, name='log_confirm'),
+    #path('log/confirm/', views.log_confirm, name='log_confirm'),
     path('log/<int:pk>/save/', views.log_save, name='log_save')
 ]
--- penaltylogger/views.py
+++ penaltylogger/views.py
@@ -27,29 +27,36 @@ def log_detail(request, pk):
     # else:
     #     form = LogForm()
     # return render(request, 'penaltylogger/log_edit.html', {'form': form})
+
 def log_new(request):
     if request.method == "GET":
         form = LogForm(request.GET)
+
         if form.is_valid():
             log = form.save(commit=False)
             player = Player.objects.filter(player_no=form.data['player_no']).first()
             log.player = player
             log.save()
             return redirect('log_confirm')
+            
+        if request.method == "POST":
+            form = LogForm(request.POST)
+            if form.is_valid():
+                context = {'form': form }
+                return render(request, "penaltylogger/log_save.html", context=context )
     else:
         form = LogForm()
     return render(request, 'penaltylogger/log_new.html', {'form': form})
 
-# 投稿確認画面（プレビュー）を表示
-def log_confirm(request):
-    if request.method == "POST":
-        form = LogForm(request.POST)
-        if form.is_valid():
-            context = {'form': form }
-            return render(request, "penaltylogger/log_save.html", context=context )
-    else:
-        form = LogForm()
-    return render(request, 'penaltylogger/log_confirm.html', {'form': form})
+# def log_confirm(request): #投稿確認画面（プレビュー）を表示
+#     if request.method == "POST":
+#         form = LogForm(request.POST)
+#         if form.is_valid():
+#             context = {'form': form }
+#             return render(request, "penaltylogger/log_save.html", context=context )
+#     else:
+#         form = LogForm()
+#     return render(request, 'penaltylogger/log_confirm.html', {'form': form})
 
 
     
