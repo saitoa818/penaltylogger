81f585d166a27e7dd2166eed12cf72f2cf0709f6
saitouakira <saitoa@cheered.jp>
2020-03-20 19:45:04 +0900

    add ログインフォームの実装
    ログインされていない場合ログインフォームを出すようにした。現在、admin以外Userがいないのでブラウザからのログインは出来ない状態。

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('login/', include('django.contrib.auth.urls')), 
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('', include('penaltylogger.urls')),
 ]
