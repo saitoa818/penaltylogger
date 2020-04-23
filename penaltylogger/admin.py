c13b41b1d55d7b67b9fe8cea9e5ad53244859512
saitouakira <saitoa@cheered.jp>
2020-04-23 14:44:15 +0900

    uppdate usernameをjudge_idで管理。登録、ログインも出来るようにした
    登録時にjudge_idがpasswordのように黒塗りに成ってしまう。要修正。

--- penaltylogger/admin.py
+++ penaltylogger/admin.py
@@ -11,26 +11,27 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-from penalty.models import Judge
+from account.models import Judge
 
 
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
+    judge_id = forms.CharField(label='judge_id', widget=forms.PasswordInput)
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    #password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = Judge
-        fields = ('judge_id',)
+        fields = ('judge_id', 'password')
 
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
+    # def clean_password2(self):
+    #     # Check that the two password entries match
+    #     password1 = self.cleaned_data.get("password1")
+    #     password2 = self.cleaned_data.get("password2")
+    #     if password1 and password2 and password1 != password2:
+    #         raise forms.ValidationError("Passwords don't match")
+    #     return password2
 
     def save(self, commit=True):
         # Save the provided password in hashed format
@@ -71,7 +72,7 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('judge_id', 'password')}),
-        ('Personal info', {'fields': ('date_of_birth',)}),
+        #('Personal info', {'fields': ('date_of_birth',)}),
         ('Permissions', {'fields': ('is_admin',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
@@ -79,7 +80,8 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ( 'password1', 'password2'),
+            'fields': ( 'judge_id', 'password1', #'password2'
+            ),
         }),
     )
     search_fields = ('judge_id',)
